e418fb2df5b5e108564728b75ceffdc95a7fd8a4
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2021-05-01 16:05:18 +1000

    SM 20210501 Added basic server framework.

--- website/app.js
+++ website/app.js
@@ -1,5 +1,13 @@
 /* Global Variables */
+let baseURL = 'http://api.animalinfo.org/data/?animal='
+let apiKey = '&appid=9f15e45060...';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+    console.log('button clicked!')
+}
